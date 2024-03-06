3c2786a08d790053f0fe272016b22206fa04b89d
«Vit-spb» <wwg79@mail.ru>
2024-03-06 13:27:18 +0300

    3

--- Tasks_003/Program.cs
+++ Tasks_003/Program.cs
@@ -1,3 +1,12 @@
+/*Реализуйте класс MatrixOperations, который содержит следующие статические методы:
+MultiplyIfPossible(int[,] matrixA, int[,] matrixB): Метод для проверки, возможно ли умножения двух матриц matrixA и matrixB. 
+Если число столбцов в матрице matrixA не равно числу строк в матрице matrixB, то выводится сообщение "It is impossible to multiply." 
+В противном случае, вызывается метод MatrixMultiplication для умножения матриц, и результат выводится с помощью метода PrintMatrix.
+MatrixMultiplication(int[,] matrixA, int[,] matrixB): Метод для умножения двух матриц matrixA и matrixB. 
+Метод возвращает новую матрицу, которая представляет собой результат умножения матрицы matrixA на матрицу matrixB.
+PrintMatrix(int[,] matrix): Метод для вывода матрицы на консоль.
+Если аргументы не переданы, программа использует матрицы по умолчанию. 
+Если аргументы переданы, программа парсит их в двумерные массивы целых чисел и выполняет умножение матриц.*/
 using System;
 
 public class Answer
@@ -33,77 +42,69 @@ public class Answer
         }
 return result;
     }
-using System;
 
-public class Answer
-{
-    public static void MultiplyIfPossible(int[,] matrixA, int[,] matrixB)
-    { // Введите свое решение ниже
-        if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    public static void PrintMatrix(int[,] matrix)
+    {  // Введите свое решение ниже
+ for (int i = 0; i < matrix.GetLength(0); i++) // строчки
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы
         {
-            Console.WriteLine("It is impossible to multiply.");
+            Console.Write($"{matrix[i, j]}\t"); // Литерал, \t = tab
+        }
+        Console.WriteLine();
+    }
+    }
+    
+ // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[,] matrix;
+
+        if (args.Length == 0)
+        {
+            // Если аргументы не переданы, используем матрицу по умолчанию
+            matrix = new int[,]
+            {
+                {5, 2},
+                {8, 1}
+            };
         }
         else
         {
-            int[,] result = MatrixMultiplication(matrixA, matrixB);
-            PrintMatrix(result);
-        }
-       }
-
-    public static int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
-    {  // Введите свое решение ниже
-
-    int[,] result = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
-        for (int i = 0; i < matrixA.GetLength(0); i++)
-        {
-            for (int j = 0; j < matrixB.GetLength(1); j++)
+            // Иначе, парсим аргументы в двумерный массив
+            string[] rows = args[0].Split(';');
+            matrix = new int[rows.Length, rows[0].Split(',').Length];
+            for (int i = 0; i < rows.Length; i++)
             {
-                int sum = 0;
-                for (int k = 0; k < matrixA.GetLength(1); k++)
+                string[] elements = rows[i].Split(',');
+                for (int j = 0; j < elements.Length; j++)
                 {
-                    sum += matrixA[i, k] * matrixB[k, j];
+                    if (int.TryParse(elements[j], out int number))
+                    {
+                        matrix[i, j] = number;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Ошибка при парсинге аргумента {elements[j]}.");
+                        return;
+                    }
                 }
-            result[i, j] = sum;
             }
         }
-return result;
+
+        Console.WriteLine("Исходная матрица:");
+        PrintMatrix(matrix);
+
+        int[,] matrixB = {
+            {5, 6},
+            {7, 8}
+        };
+
+        Console.WriteLine("\nMatrix B:");
+        PrintMatrix(matrixB);
+
+        Console.WriteLine("\nMultiplication result:");
+
+        MultiplyIfPossible(matrix, matrixB);
     }
-
-void PrintMatrix(int[,] matr)
-{
-for (int i = 0; i < matr.GetLength(0); i++) // строчки
-{
-for (int j = 0; j < matr.GetLength(1); j++) // столбцы
-{
-Console.Write($"{matr[i, j]}\t"); // Литерал, \t = tab
 }
-Console.WriteLine();
-}
-}
-
-void ChangeRows(int[,] matr)
-{
-int indexLastRow = matr.GetLength(0) - 1; // Индекс посл. стр
-// 1 2 3
-// ..........
-// 4 5 6
-for (int i = 0; i < matr.GetLength(1); i++)
-{
-int temp = matr[0, i]; // temp = matr[0,0]; temp = 1
-matr[0, i] = matr[indexLastRow, i]; // matr[0,0] = 4
-// 4 2 3
-// ..........
-// 4 5 6
-matr[indexLastRow, i] = temp; // temp = 1
-// 4 2 3
-// ..........
-// 1 5 6
-
-}
-}
-
-int[,] array2D = GetMatrix(rows, columns, 0, 10);
-PrintMatrix(array2D);
-Console.WriteLine("Результат: ");
-ChangeRows(array2D);
-PrintMatrix(array2D);
